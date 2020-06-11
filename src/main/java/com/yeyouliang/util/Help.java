a30cddd1335425397a0381fe417b18f567dccad7
yeyouliang <1538520193@qq.com>
2020-06-11 09:23:54 +0800

    对正整数分解质因数

--- src/main/java/com/yeyouliang/util/Help.java
+++ src/main/java/com/yeyouliang/util/Help.java
@@ -1,8 +1,38 @@
 package com.yeyouliang.util;
 
+import org.junit.Test;
+
 /**
  * Created by YEYOULIANG on 2020/5/2.
  */
 public class Help {
 
+    @Test
+    public void tests() {
+        int num=234;
+        System.out.print(num+"=");
+        fjzys(num);
+    }
+
+    public void fjzys(int num) {
+        int pfg = (int) Math.sqrt(num);
+        if (pfg < 2) {
+            System.out.println(num);
+            return;
+        }
+        int shang = 0;
+        for (int i = 2; i <= pfg; i++) {
+            int yu = num % i;
+            if (yu == 0) {
+                System.out.print(i + "*");
+                shang = num / i;
+                break;
+            }
+        }
+        if (shang == 0) {
+            System.out.println(num);
+        } else {
+            fjzys(shang);
+        }
+    }
 }
