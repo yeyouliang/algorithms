8391c5275306cef2eb403ad93329cce2a6bb046d
youliang.ye <1538520193@qq.com>
2021-04-17 14:32:22 +0800

    蓝桥杯编程题：寒假作业（补充）

--- src/main/java/com/yeyouliang/lanqiao/WinterVacationTask.java
+++ src/main/java/com/yeyouliang/lanqiao/WinterVacationTask.java
@@ -1,5 +1,10 @@
 package com.yeyouliang.lanqiao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by YYL on 2021/4/15 : 22:26.
  */
@@ -22,18 +27,44 @@ public class WinterVacationTask {
      * 3×4=12
      * 10/2=5
      * 就算两种解法（加法，乘法交换律后算不同的方案）。你一共找到了多少种方案？
-     * */
+     */
     public static void main(String[] args) {
-        int a=13/2;
-        for (int i = 2; i <= a; i++) {
-            for (int j = i*2; j < 14; j++) {
-                int yu=j%i;
-                int shang=j/i;
-                if (j!=0 || shang==i){
-                    continue;
-                }
+        List<Integer> origin = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13).collect(Collectors.toList());
+        List<Integer> has = new ArrayList<>();
+        int[] count = new int[1];
+        wvt(origin, has, count);
+        System.out.println(count[0]);
+    }
 
+    public static void wvt(List<Integer> origin, List<Integer> has, int[] count) {
+        if (has.size() == 3) {
+            if (has.get(0) + has.get(1) != has.get(2)) {
+                return;
             }
+        }
+        if (has.size() == 6) {
+            if (has.get(3) < has.get(4) || has.get(3) - has.get(4) != has.get(5)) {
+                return;
+            }
+        }
+        if (has.size() == 9) {
+            if (has.get(6) * has.get(7) != has.get(8)) {
+                return;
+            }
+        }
+        if (has.size() == 12) {
+            if (has.get(9) > has.get(10) && has.get(9) % has.get(10) == 0 && has.get(9) / has.get(10) == has.get(11)) {
+                count[0]++;
+                System.out.println(has);
+            }
+            return;
+        }
+        for (Integer integer : origin) {
+            List<Integer> o = new ArrayList<>(origin);
+            o.remove(integer);
+            List<Integer> h = new ArrayList<>(has);
+            h.add(integer);
+            wvt(o, h, count);
         }
     }
 }
