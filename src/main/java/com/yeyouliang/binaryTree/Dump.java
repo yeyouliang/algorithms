0b5192bbfd27303f67c0332b12de84163eb8ca80
youliang.ye <1538520193@qq.com>
2021-04-24 15:02:28 +0800

    java算法基础：根据整数数组构建二叉树

--- src/main/java/com/yeyouliang/binaryTree/Dump.java
+++ src/main/java/com/yeyouliang/binaryTree/Dump.java
@@ -1,61 +1,57 @@
 package com.yeyouliang.binaryTree;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by YYL on 2021/4/22 : 20:39.
  */
 public class Dump {
 
-    public static void main(String[] args) {
-        int []ints={2,1,5,4,9,15,12,11,8,16,7};
+    public static List<TreeNode> list = new ArrayList<>();
 
+    public static void main(String[] args) {
+        //int []ints={2,1,5,4,9,15,12,11,8,16,7};
+        int[] ints = {7, 13, 4, 5, 8, 1, 11, 9};
+        TreeNode root = new TreeNode(ints[0], null, null);
+        list.add(root);
+        for (int i = 1; i < ints.length; i++) {
+            build(ints[i]);
+        }
+        System.out.println(root.getValue());
     }
 
-    public static void max(TreeNode root,int i){
-        int rv=root.getValue();
-        if (i>rv){
+    /**
+     * 根据整数数组构建二叉树
+     * */
+    public static void build(int num) {
+        while (true) {
+            for (TreeNode treeNode : list) {
+                TreeNode left = treeNode.getLeft();
+                TreeNode right = treeNode.getRight();
+                if (left == null) {
+                    TreeNode le = new TreeNode(num, null, null);
+                    treeNode.setLeft(le);
+                    list.add(le);
+                    return;
+                }
+                if (right == null) {
+                    TreeNode ri = new TreeNode(num, null, null);
+                    treeNode.setRight(ri);
+                    list.add(ri);
+                    return;
+                }
+            }
+            return;
+        }
+    }
+
+    public static void max(TreeNode root, int i) {
+        int rv = root.getValue();
+        if (i > rv) {
             root.setValue(i);
 
-        }
-    }
-
-    public static class TreeNode {
-        private int value;
-        private TreeNode left; // 左子树
-        private TreeNode right; // 右子树
-
-        public TreeNode(int value, TreeNode left, TreeNode right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-
-        public TreeNode getLeft() {
-            return left;
-        }
-
-        public void setLeft(TreeNode left) {
-            this.left = left;
-        }
-
-        public TreeNode getRight() {
-            return right;
-        }
-
-        public void setRight(TreeNode right) {
-            this.right = right;
-        }
-
-        @Override
-        public String toString() {
-            return this.getValue() + "";
         }
     }
 }
