cb927226c32fd7b161cc89a6ed0a226795393e0c
youliang.ye <1538520193@qq.com>
2021-05-04 22:35:32 +0800

    算法编程题：最长公共子串

---
+++ src/main/java/com/yeyouliang/others/PublicChild.java
@@ -0,0 +1,12 @@
+package com.yeyouliang.others;
+
+/**
+ * Created by YYL on 2021/4/22 : 20:34.
+ */
+public class PublicChild {
+    public static void main(String[] args) {//abcd
+        String a="abcdkkk";
+        String b="baabcdadabc";
+
+    }
+}
