bbe39b1989a80f1c015723c291768fece43d9d2a
youliang.ye <1538520193@qq.com>
2021-06-07 21:48:30 +0800

    算法编程题：求解数组中反序对的个数

---
+++ src/main/java/com/yeyouliang/array/StackLickQueue.java
@@ -0,0 +1,18 @@
+package com.yeyouliang.array;
+
+/**
+ * Created by YYL on 2021/6/7 : 21:45.
+ */
+public class StackLickQueue {
+    /**
+     * 用两个栈模拟队列操作
+     * 假设使用栈A与栈B模拟队列Q，A为插入栈，B为弹出栈，以实现队列Q。
+     * 再假设A和B都为空，可以认为栈A提供入队列的功能，栈B提供出队列的功能。
+     * 要入队列，入栈A即可，而出队列则需要分两种情况考虑：
+     * 1）若栈B不为空，则直接弹出栈B的数据。
+     * 2）若栈B为空，则依次弹出栈A的数据，放入栈B中，再弹出栈B的数据。
+     * */
+    public static void main(String[] args) {
+        
+    }
+}
