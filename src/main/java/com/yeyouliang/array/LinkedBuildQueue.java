9cfb698b6a994c5392ddd35dd0d7be3f006883ca
youliang.ye <1538520193@qq.com>
2021-06-08 20:50:18 +0800

    算法编程题：采用链表方式实现队列

---
+++ src/main/java/com/yeyouliang/array/LinkedBuildQueue.java
@@ -0,0 +1,13 @@
+package com.yeyouliang.array;
+
+/**
+ * Created by YYL on 2021/6/8 : 20:49.
+ */
+public class LinkedBuildQueue {
+    /**
+     * 采用链表方式实现队列
+     * */
+    public static void main(String[] args) {
+
+    }
+}
