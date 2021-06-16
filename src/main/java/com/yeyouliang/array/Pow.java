5085e373cf0607b5570adb3d5f0ecc22bed5e853
youliang.ye <1538520193@qq.com>
2021-06-17 07:57:37 +0800

    算法编程题：如何判断一个数是否为2的n次方

---
+++ src/main/java/com/yeyouliang/array/Pow.java
@@ -0,0 +1,13 @@
+package com.yeyouliang.array;
+
+/**
+ * Created by YYL on 2021/6/17 : 7:56.
+ */
+public class Pow {
+    /**
+     * 如何判断一个数是否为2的n次方
+     * */
+    public static void main(String[] args) {
+
+    }
+}
