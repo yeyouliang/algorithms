a794746d096f514f553aeefa19f385d10391ef18
youliang.ye <1538520193@qq.com>
2021-06-23 20:35:21 +0800

    算法编程题：将链表反转

--- src/main/java/com/yeyouliang/linked/LinkedReverse.java
+++ src/main/java/com/yeyouliang/linked/LinkedReverse.java
@@ -8,6 +8,18 @@ public class LinkedReverse {
      * 实现链表的反转
      * */
     public static void main(String[] args) {
-
+        int[] a = {1, 2, 3, 4, 5};
+        LinkedInt la = LinkedInt.init(a);
+        LinkedInt next = la.getNext();
+        LinkedInt linkedInt = new LinkedInt(la.getValue());
+        LinkedInt origin = linkedInt;
+        while (true) {
+            if (next.getValue() == la.getValue()) {
+                break;
+            }
+            linkedInt = new LinkedInt(next.getValue(), linkedInt);
+            next = next.getNext();
+        }
+        origin.setNext(linkedInt);
     }
 }
