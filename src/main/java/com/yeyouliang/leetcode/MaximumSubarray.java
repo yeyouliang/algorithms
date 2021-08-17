cdd9a3ae5d09a991c0e0e8017e19e6fbadce9183
youliang.ye <1538520193@qq.com>
2021-08-17 21:51:12 +0800

    LeetCode 53 : 最大子序和（更新）

--- src/main/java/com/yeyouliang/leetcode/MaximumSubarray.java
+++ src/main/java/com/yeyouliang/leetcode/MaximumSubarray.java
@@ -8,24 +8,38 @@ import java.util.List;
  * Created by YYL on 2021/3/14 : 14:13.
  */
 public class MaximumSubarray {
-    public static void main(String[] args) {
-        int []ints={-2,1,-3,4,-1,2,1,-5,4};
-        List<Integer> list=new ArrayList<>();//1,3,5,6,8
-        for (int i = 0; i < ints.length; i++) {
-            if (ints[i]>0){
-                list.add(i);
+    /**
+     * 53. 最大子序和
+     */
+    public static void main(String[] args) {//4,-1,2,1
+        int[][] nums = {{-2, 1, -3, 4, -1, 2, 1, -5, 4},{5,4,-1,7,8},{8,-19,5,-4,20},{1,2,-1,-2,2,1,-2,1,4,-5,4}};
+        //int[][] nums = {{1,2,-1,-2,2,1,-2,1,4,-5,4}};
+        for (int[] num : nums) {
+            System.out.println(Arrays.toString(num) + "," + maxSubarray(num));
+        }
+    }
+
+    private static int maxSubarray(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] ints = new int[nums.length];
+        int b = nums[0];
+        ints[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= 0) {
+                b += nums[i];
+                ints[i] = Math.max(nums[i], ints[i - 1]);
+            } else {
+                if (b <= 0) {
+                    ints[i] = Math.max(nums[i], ints[i - 1]);
+                    b=nums[i];
+                } else {
+                    ints[i] = Math.max(nums[i] + b, ints[i - 1]);
+                    b = nums[i] + b;
+                }
             }
         }
-        List<Integer> add=new ArrayList<>();
-        for (int i = 0; i < list.size()-1; i++) {
-            int count=0;
-            for (int j = list.get(i); j < list.get(i+1); j++) {
-                count+=ints[j];
-            }
-            add.add(ints[list.get(i)]);
-            add.add(count);
-        }
-        add.add(ints[list.get(list.size()-1)]);
-        System.out.println(Arrays.toString(add.toArray()));
+        return ints[ints.length - 1];
     }
 }
