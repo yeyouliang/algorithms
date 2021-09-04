d6f30d37cb86907fa42d82ae1e28a7950e46e8d1
youliang.ye <1538520193@qq.com>
2021-09-04 22:43:09 +0800

    LeetCode 1. 两数之和

--- src/main/java/com/yeyouliang/leetcode/TwoSum.java
+++ src/main/java/com/yeyouliang/leetcode/TwoSum.java
@@ -1,5 +1,6 @@
 package com.yeyouliang.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,18 +8,33 @@ import java.util.Map;
  * Created by YYL on 2021/2/1 : 20:12.
  */
 public class TwoSum {
+    /**
+     * 1. 两数之和
+     * */
     public static void main(String[] args) {
-        int[] ints = {2, 7, 11, 15, 25, 19, 38, 145};
-        int target = 117;
+        int[][][] nums = {
+                {{2, 7, 11, 15}, {9}},
+                {{3, 2, 4}, {6}},
+                {{3, 3}, {6}}
+        };
+        for (int[][] num : nums) {
+            System.out.println(Arrays.toString(num[0]) + "" + Arrays.toString(twoSum(num[0], num[1][0])));
+        }
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        int[] ints = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < ints.length; i++) {
-            if (target != ints[i] && map.containsKey(ints[i])) {
-                System.out.println(i + "," + map.get(ints[i]));
-                return;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                ints[0] = i;
+                ints[1] = map.get(nums[i]);
+                break;
             } else {
-                map.put(target - ints[i], i);
+                int diff = target - nums[i];
+                map.put(diff, i);
             }
         }
-        System.out.println("none");
+        return ints;
     }
 }
