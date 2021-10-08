d7452c4a2e8ab9913f66b5bca0d4552590737a4d
youliang.ye <1538520193@qq.com>
2021-10-08 20:16:08 +0800

    LeetCode 387. 字符串中的第一个唯一字符（更新）

--- src/main/java/com/yeyouliang/leetcode/FirstUniqueCharacterInAString.java
+++ src/main/java/com/yeyouliang/leetcode/FirstUniqueCharacterInAString.java
@@ -1,5 +1,10 @@
 package com.yeyouliang.leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by YYL on 2021/10/7 : 13:30.
  */
@@ -21,14 +26,28 @@ public class FirstUniqueCharacterInAString {
     }
 
     private static int firstUniqChar(String s) {
-        int index = -1;
+        Map<Character, List<Integer>> map = new LinkedHashMap<>();
         if (s.length() > 0) {
-            for (int i = 0; i < s.split("").length; i++) {
-                String a = String.valueOf(s.charAt(i));
-                int first = s.indexOf(a);
-                int last = s.lastIndexOf(a);
-                if (first == last) {
-                    index = i;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (map.containsKey(c)) {
+                    List<Integer> list = map.get(c);
+                    Integer integer = list.get(1);
+                    list.remove(1);
+                    list.add(integer + 1);
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(i);
+                    list.add(1);
+                    map.put(c, list);
+                }
+            }
+        }
+        int index = -1;
+        if (!map.isEmpty()) {
+            for (List<Integer> list : map.values()) {
+                if (list.get(1) == 1) {
+                    index = list.get(0);
                     break;
                 }
             }
