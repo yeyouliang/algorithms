70a02264b0636a78c14512db40005749c3c50039
youliang.ye <1538520193@qq.com>
2021-10-29 14:18:14 +0800

    剑指 Offer II 001. 整数除法

---
+++ src/main/java/com/yeyouliang/jianzhi/Offer_2_001.java
@@ -0,0 +1,92 @@
+package com.yeyouliang.jianzhi;
+
+import java.util.Arrays;
+
+/**
+ * Created by YYL on 2021/10/29 : 11:03.
+ */
+public class Offer_2_001 {
+    /**
+     * 剑指 Offer II 001. 整数除法
+     * */
+    public static void main(String[] args) {
+        int[][] ints = {
+                {7, -3},
+                {0, 1},
+                {1, 1},
+                {-17, 2},
+                {-16, 2},
+                {-15, 2},
+                {-14, 2},
+                {-13, 2},
+                {-12, 2},
+                {-11, 2},
+                {-10, 2},
+                {-9, 2},
+                {-8, 2},
+                {-7, 2},
+                {-6, 2},
+                {-5, 2},
+                {-4, 2},
+                {-3, 2},
+                {-2, 2},
+                {-1, 2},
+                {0, 2},
+                {1, 2},
+                {2, 2},
+                {3, 2},
+                {4, 2},
+                {5, 2},
+                {6, 2},
+                {7, 2},
+                {8, 2},
+                {9, 2},
+                {10, 2},
+                {11, 2},
+                {12, 2},
+                {13, 2},
+                {14, 2},
+                {15, 2},
+                {16, 2},
+                {17, 2},
+        };
+        for (int[] anInt : ints) {
+            System.out.println(Arrays.toString(anInt) + ":" + divide(anInt[0], anInt[1]));
+        }
+    }
+
+    private static int divide(int a, int b) {
+        if (a == 0) {
+            return 0;
+        } else {
+            long c = Math.abs(a);
+            long d = Math.abs(b);
+            if (c < d) {
+                return 0;
+            } else if (a == b) {
+                return 1;
+            } else if (c == d) {
+                return -1;
+            } else {
+                boolean zheng = (a < 0 && b < 0) || (a > 0 && b > 0);
+                long e = d;
+                int g = 1;
+                while (true) {
+                    e = e + e;
+                    g = g + g;
+                    if (e == c) {
+                        return zheng ? g : 0 - g;
+                    } else if (e > c) {
+                        while (true) {
+                            e -= d;
+                            g--;
+                            if (e <= c) {
+                                return zheng ? g : 0 - g;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
