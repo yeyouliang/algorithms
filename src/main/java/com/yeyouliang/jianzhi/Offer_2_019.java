765409ae28571f6058d12ef25cc7e15f7bea945c
youliang.ye <1538520193@qq.com>
2021-10-29 21:22:07 +0800

    剑指 Offer II 019. 最多删除一个字符得到回文PLUS

--- src/main/java/com/yeyouliang/jianzhi/Offer_2_019.java
+++ src/main/java/com/yeyouliang/jianzhi/Offer_2_019.java
@@ -6,9 +6,30 @@ package com.yeyouliang.jianzhi;
 public class Offer_2_019 {
     /**
      * 剑指 Offer II 019. 最多删除一个字符得到回文
-     * */
+     */
     public static void main(String[] args) {
         String[] strings = {
+                "ebcbbececabbacecbbcbe",
+                "xabccba",
+                "xabcba",
+                "xaa",
+                "axa",
+                "aax",
+                "baca",
+                "abca",
+                "acba",
+                "acab",
+                "xacca",
+                "axcca",
+                "acxca",
+                "accxa",
+                "accax",
+                "xabcba",
+                "axbcba",
+                "abxcba",
+                "abcxba",
+                "abcbxa",
+                "abcbax",
                 "aba",
                 "abca",
                 "abc",
@@ -21,22 +42,46 @@ public class Offer_2_019 {
     }
 
     private static boolean validPalindrome(String s) {
-        boolean falg = false;
         if (s.length() < 3) {
-            falg = true;
+            return true;
         } else {
             if (s.equals(new StringBuilder(s).reverse().toString())) {
-                falg = true;
+                return true;
             } else {
-                for (int i = 0; i < s.length(); i++) {
-                    String a = s.substring(0, i) + s.substring(i + 1, s.length());
-                    if (a.equals(new StringBuilder(a).reverse().toString())) {
-                        falg = true;
-                        break;
+                int a = 0;
+                int b = s.length() - 1;
+                while (a < b) {
+                    if (s.charAt(a) == s.charAt(b)) {
+                        a++;
+                        b--;
+                    } else {
+                        if (a + 1 == b) {
+                            return true;
+                        }
+                        int c;
+                        int d;
+                        if (s.charAt(a) == s.charAt(b - 1)) {
+                            c = a;
+                            d = b - 1;
+                        } else if (s.charAt(a + 1) == s.charAt(b)) {
+                            c = a + 1;
+                            d = b;
+                        } else {
+                            return false;
+                        }
+                        while (c < d) {
+                            if (s.charAt(c) == s.charAt(d)) {
+                                c++;
+                                d--;
+                            } else {
+                                return false;
+                            }
+                        }
+                        return true;
                     }
                 }
+                return true;
             }
         }
-        return falg;
     }
 }
