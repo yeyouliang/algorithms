a85f1d43d27d9ba6a66a10ba136340e97d47ea91
youliang.ye <1538520193@qq.com>
2021-11-07 22:26:08 +0800

    排序算法PLUS

--- src/main/java/com/yeyouliang/sort/NewSort.java
+++ src/main/java/com/yeyouliang/sort/NewSort.java
@@ -1,6 +1,8 @@
 package com.yeyouliang.sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by YYL on 2021/11/7 : 14:11.
@@ -9,6 +11,13 @@ public class NewSort {
 
     public static void main(String[] args) {
         int[][] ints = {
+                {1},
+                {1, 2},
+                {1, 2, 3},
+                {1, 2, 3, 4},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5, 6},
+                {1, 2, 3, 4, 5, 6, 7},
                 {5, 1, 3, 9, 8, 2, 6, 4, 7},
                 {2},
                 {2, 1},
@@ -20,7 +29,59 @@ public class NewSort {
                 {5, 1, 5, 2, 5, 3, 5, 4},
         };
         for (int[] anInt : ints) {
-            System.out.println(Arrays.toString(anInt) + ":" + Arrays.toString(kuaipai(anInt)));
+            System.out.println(Arrays.toString(anInt) + ":" + Arrays.toString(heap(anInt)));
+        }
+    }
+
+    private static int[] heap(int[] ints) {
+        if (ints != null && ints.length > 1) {
+            List<TreeNode> a = new ArrayList<>();
+            for (int anInt : ints) {
+                a.add(new TreeNode(anInt));
+            }
+            int b = 0;
+            while (true) {
+                int c = b * 2 + 1;
+                if (c < ints.length) {
+                    a.get(b).left = a.get(c);
+                    if (c + 1 < ints.length) {
+                        a.get(b).right = a.get(c + 1);
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                b++;
+            }
+            System.out.println(a.size());
+        }
+        return ints;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    '}';
         }
     }
 
@@ -53,13 +114,54 @@ public class NewSort {
                 }
             }
             ints[c] = ints[0];
-            int []e=new int[a.length];
-            System.arraycopy(a,0,e,0,a.length);
-            int []f=new int[b.length];
-            System.arraycopy(b,0,f,0,b.length);
-            System.arraycopy(e, 0, ints, 0, c);
-            System.arraycopy(f, 0, ints, c+1, d);
+            int[] e = new int[c];
+            System.arraycopy(a, 0, e, 0, c);
+            int[] f = new int[d];
+            System.arraycopy(b, 0, f, 0, d);
+            int[] g = kuaipai(e);
+            int[] h = kuaipai(f);
+            System.arraycopy(g, 0, ints, 0, c);
+            System.arraycopy(h, 0, ints, c + 1, d);
         }
         return ints;
     }
+
+    private static int[] zhicha(int[] ints) {
+        if (ints != null && ints.length > 1) {
+            for (int i = 1; i < ints.length; i++) {
+                for (int j = i; j > 0; j--) {
+                    if (ints[j - 1] > ints[j]) {
+                        int temp = ints[j - 1];
+                        ints[j - 1] = ints[j];
+                        ints[j] = temp;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return ints;
+    }
+
+    private static int[] selection(int[] ints) {
+        if (ints != null && ints.length > 1) {
+            for (int i = 0; i < ints.length - 1; i++) {
+                int a = i;
+                int b = ints[i];
+                for (int j = i + 1; j < ints.length; j++) {
+                    if (ints[j] < b) {
+                        a = j;
+                        b = ints[j];
+                    }
+                }
+                if (a != i) {
+                    int temp = ints[i];
+                    ints[i] = ints[a];
+                    ints[a] = temp;
+                }
+            }
+        }
+        return ints;
+    }
+
 }
