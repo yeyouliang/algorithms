74654396e144e62143185a601a6a96d3d87ed3e1
youliang.ye <1538520193@qq.com>
2021-11-13 10:14:34 +0800

    排序算法-归并排序

--- src/main/java/com/yeyouliang/sort/NewSort.java
+++ src/main/java/com/yeyouliang/sort/NewSort.java
@@ -12,7 +12,7 @@ public class NewSort {
     public static void main(String[] args) {
         int[][] ints = {
                 {8, 9, 1, 7, 2, 3, 5, 4, 6, 0},
-                /*{3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48},
+                {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48},
                 {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48, 100, 10000},
                 {4, 1, 9, 3, 7, 8, 5, 6, 2},
                 {4, 6, 8, 5, 9},
@@ -31,11 +31,54 @@ public class NewSort {
                 {2, 5, 3, 1, 4},
                 {1, 2, 3, 3},
                 {2, 1, 2, 5, 3, 2},
-                {5, 1, 5, 2, 5, 3, 5, 4},*/
+                {5, 1, 5, 2, 5, 3, 5, 4},
         };
         for (int[] anInt : ints) {
-            System.out.println(Arrays.toString(anInt) + ":" + Arrays.toString(shell(anInt)));
+            System.out.println(Arrays.toString(anInt) + ":" + Arrays.toString(guibing(anInt)));
         }
+    }
+
+    private static int[] guibing(int[] ints) {
+        if (ints != null && ints.length > 1) {
+            if (ints.length == 2) {
+                if (ints[0] > ints[1]) {
+                    int temp = ints[0];
+                    ints[0] = ints[1];
+                    ints[1] = temp;
+                }
+            } else {
+                int a = ints.length / 2;
+                int[] b = new int[a];
+                int[] c = new int[ints.length - a];
+                System.arraycopy(ints, 0, b, 0, b.length);
+                System.arraycopy(ints, a, c, 0, c.length);
+                int[] d = guibing(b);
+                int[] e = guibing(c);
+                int f = 0;
+                int g = 0;
+                int h = 0;
+                while (true) {
+                    if (g == d.length) {
+                        for (int i = h; i < e.length; i++) {
+                            ints[f++] = e[i];
+                        }
+                        break;
+                    }
+                    if (h == e.length) {
+                        for (int i = g; i < d.length; i++) {
+                            ints[f++] = d[i];
+                        }
+                        break;
+                    }
+                    if (d[g] < e[h]) {
+                        ints[f++] = d[g++];
+                    } else {
+                        ints[f++] = e[h++];
+                    }
+                }
+            }
+        }
+        return ints;
     }
 
     private static int[] shell(int[] ints) {
