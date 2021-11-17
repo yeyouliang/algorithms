933b19b4ebd60d889d752b75f4f640d385f6be1c
youliang.ye <1538520193@qq.com>
2021-11-17 15:25:07 +0800

    剑指 Offer II 024. 反转链表

---
+++ src/main/java/com/yeyouliang/jianzhi/Offer_2_024.java
@@ -0,0 +1,47 @@
+package com.yeyouliang.jianzhi;
+
+/**
+ * Created by YYL on 2021/10/28 : 20:40.
+ */
+public class Offer_2_024 {
+    /**
+     * 剑指 Offer II 024. 反转链表
+     * */
+    public static void main(String[] args) {
+        ListNode l5 = new ListNode(5);
+        ListNode l4 = new ListNode(4, l5);
+        ListNode l3 = new ListNode(3, l4);
+        ListNode l2 = new ListNode(2, l3);
+        ListNode l1 = new ListNode(1, l2);
+        ListNode listNode = reverseList(l1);
+        System.out.println(listNode.val);
+    }
+
+    private static ListNode reverseList(ListNode head) {
+        if (head != null) {
+            ListNode next = head.next;
+            if (next != null) {
+                ListNode listNode = reverseList(next);
+                ListNode b = listNode;
+                while (b.next != null) {
+                    b = b.next;
+                }
+                b.next = new ListNode(head.val);
+                head = listNode;
+            }
+        }
+        return head;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+    }
+}
