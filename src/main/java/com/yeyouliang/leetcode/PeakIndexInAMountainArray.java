a9196b638aa02c15799aeaca73ccb27b69b11c46
youliang.ye <1538520193@qq.com>
2021-11-17 15:16:48 +0800

    852. 山脉数组的峰顶索引

---
+++ src/main/java/com/yeyouliang/leetcode/PeakIndexInAMountainArray.java
@@ -0,0 +1,61 @@
+package com.yeyouliang.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by YYL on 2021/11/1 : 9:52.
+ */
+public class PeakIndexInAMountainArray {
+    /**
+     * 852. 山脉数组的峰顶索引
+     * */
+    public static void main(String[] args) {
+        int[][] ints = {
+                {3, 2, 1},
+                {2, 3, 1},
+                {1, 2, 3},
+                {4, 3, 2, 1},
+                {3, 4, 2, 1},
+                {2, 3, 4, 1},
+                {1, 2, 3, 4},
+                {5, 4, 3, 2, 1},
+                {4, 5, 3, 2, 1},
+                {3, 4, 5, 2, 1},
+                {2, 3, 4, 5, 1},
+                {1, 2, 3, 4, 5},
+                {6, 5, 4, 3, 2, 1},
+                {5, 6, 4, 3, 2, 1},
+                {4, 5, 6, 3, 2, 1},
+                {3, 4, 5, 6, 2, 1},
+                {2, 3, 4, 5, 6, 1},
+                {1, 2, 3, 4, 5, 6},
+                {0, 1, 0},
+                {1, 3, 5, 4, 2},
+                {0, 10, 5, 2},
+                {3, 4, 5, 1},
+                {24, 69, 100, 99, 79, 78, 67, 36, 26, 19},
+        };
+        for (int[] anInt : ints) {
+            System.out.println(Arrays.toString(anInt) + ":" + peakIndexInMountainArray(anInt));
+        }
+    }
+
+    private static int peakIndexInMountainArray(int[] arr) {
+        int a = 0;
+        int b = arr.length - 1;
+        while (true) {
+            int c = b - a;
+            if (c == 1) {
+                return arr[a] > arr[b] ? a : b;
+            } else {
+                int d = c / 2;
+                int e = a + d;
+                if (arr[e] > arr[e - 1]) {
+                    a = e;
+                } else {
+                    b = e;
+                }
+            }
+        }
+    }
+}
