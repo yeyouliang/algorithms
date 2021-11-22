d12fdc5b838b23b12620358ef5cd9a0dce87577b
youliang.ye <1538520193@qq.com>
2021-11-22 09:14:36 +0800

    二叉树的深度优先遍历（DFS）

---
+++ src/main/java/com/yeyouliang/tree/Dfs.java
@@ -0,0 +1,48 @@
+package com.yeyouliang.tree;
+
+import java.util.Stack;
+
+/**
+ * Created by YYL on 2021/11/21 : 19:59.
+ */
+public class Dfs {
+    /**
+     * 二叉树的深度优先遍历（DFS）
+     */
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t13 = new TreeNode(13);
+        TreeNode t16 = new TreeNode(16);
+        TreeNode t21 = new TreeNode(21);
+        TreeNode t30 = new TreeNode(30);
+        TreeNode t2 = new TreeNode(2, t1, null);
+        TreeNode t5 = new TreeNode(5, t4, t6);
+        TreeNode t15 = new TreeNode(15, t13, t16);
+        TreeNode t29 = new TreeNode(29, t21, t30);
+        TreeNode t3 = new TreeNode(3, t2, t5);
+        TreeNode t20 = new TreeNode(20, t15, t29);
+        TreeNode t8 = new TreeNode(8, t3, t20);
+        m1(t8);
+    }
+
+    private static void m1(TreeNode head) {
+        if (head != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            stack.add(head);
+            while (!stack.empty()) {
+                TreeNode treeNode = stack.pop();
+                TreeNode left = treeNode.left;
+                TreeNode right = treeNode.right;
+                System.out.print(treeNode.val + ",");
+                if (right != null) {
+                    stack.add(right);
+                }
+                if (left != null) {
+                    stack.add(left);
+                }
+            }
+        }
+    }
+}
